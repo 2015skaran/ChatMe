492ceff719b36ecb9d3ced3a13efab70b14815fe
Srijan Karan <srijankaran@Srijans-MacBook-Pro.local>
2016-11-19 17:44:43 -0500

    first commit

---
+++ App.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Firebase from './firebase-wrapper'; // Import Firebase library
+import Message from '.components/Message.jsx';
+import Input from '.components/Input.jsx';
+
+const App = React.createClass({
+  getInitialState() {
+    return {
+      messages: [] // Initialize empty list of messages
+    };
+  },
+
+  componentWillMount() {
+    Firebase.turnOn(newState => {
+      this.setState(newState);
+    });
+  },
+
+  componentWillUnmount() {
+    Firebase.turnOff();
+  },
+
+  renderMessageDiv(message) {
+    return <Message key=message.key} message=message.message/>;
+  },
+
+  render() {
+    // Iterates through the messages in state to create HTML elements
+    // for each message
+    const messageDivs = this.state.messages.map(this.renderMessageDiv);
+
+    return (
+      <div>
+        <h2>ChatMe</h2>
+          <div>
+ +          {messageDivs}
+ +        </div>
+      </div>
+    );
+  }
+});
+
+export default App;
---
+++ components/Message.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Message = React.createClass({
+  // Setting propTypes ensure that your component is used correctly
+  propTypes: {
+    message: React.PropTypes.object
+  },
+
+  render() {
+  	const {name, message} = this.props.message;
+  	 retur <p> {name} : {message} </p>
+  }
+});
+
+export default Message;
